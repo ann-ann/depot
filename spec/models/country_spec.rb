f574a9174352870a8aaffa44e457d3b9a48f0479
anna-vasilenko <ann@anns-MacBook-Air.local>
2014-01-09 00:26:54 +0200

    added validation and specs for country. TODO fix credit_card validator and specs. TODO books rating

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Country do
+  
+  let(:country) {FactoryGirl.create :country}
 
+  context "validations" do  
+    it { expect(country).to validate_presence_of(:name)}
+    it { expect(country).to validate_uniqueness_of(:name)}
+  end
 end
